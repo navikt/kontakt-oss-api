3a2069e685017d9b895e8a06056f911634c56fa6
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-10-29 12:35:49 +0100

    initial commit

---
+++ src/main/java/no/nav/tag/interessemelding/InteressemeldingApplication.java
@@ -0,0 +1,12 @@
+package no.nav.tag.interessemelding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteressemeldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteressemeldingApplication.class, args);
+	}
+}
---
+++ src/test/java/no/nav/tag/interessemelding/InteressemeldingApplicationTests.java
@@ -0,0 +1,16 @@
+package no.nav.tag.interessemelding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InteressemeldingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
