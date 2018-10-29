02584cc86bce998e914c2b26cc554cad39f18ece
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-10-29 12:46:22 +0100

    cleanup

--- src/main/java/no/nav/tag/interessemelding/InteressemeldingApplication.java
+++ src/main/java/no/nav/tag/interessemelding/InteressemeldingApplication.java
@@ -2,11 +2,19 @@ package no.nav.tag.interessemelding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class InteressemeldingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InteressemeldingApplication.class, args);
 	}
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 }
