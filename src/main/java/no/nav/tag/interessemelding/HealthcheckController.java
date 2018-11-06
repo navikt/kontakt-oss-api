660514d51af18e3b627a337d3313c895f70c3445
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-11-06 11:59:17 +0100

    Oppdater healthcheck

--- src/main/java/no/nav/tag/interessemelding/HealthcheckController.java
+++ src/main/java/no/nav/tag/interessemelding/HealthcheckController.java
@@ -5,17 +5,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HealthcheckController {
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/isAlive", method = RequestMethod.GET)
     @ResponseBody
-    public Interessemelding isAlive() {
-        return new Interessemelding();
+    public String isAlive() {
+        return "ok";
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/isReady", method = RequestMethod.GET)
     @ResponseBody
-    public Interessemelding isReady() {
-        return new Interessemelding();
+    public String isReady() {
+        return "ok";
     }
 }
