72cb2b7abc7e19bbd1d29ee2253201f7e5f4a804
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-11-19 15:47:29 +0100

    add origins from Vault

---
+++ src/main/java/no/nav/tag/kontaktskjema/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package no.nav.tag.kontaktskjema;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Value("${cors_origins}")
+    private String origins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(origins);
+            }
+        };
+    }
+}
