4df07246f6daaaff1d183c442913e812783f84c7
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-11-19 14:39:44 +0100

    testing cors

--- src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaController.java
+++ src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "https://tjenester-t1.nav.no")
 @RestController
 public class KontaktskjemaController {
 
