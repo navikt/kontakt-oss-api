379d4ec63473331c84c7f4fd237b281b16f0c4bf
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-11-26 09:57:32 +0100

    Legg til melding i db

--- src/main/java/no/nav/tag/kontaktskjema/Kontaktskjema.java
+++ src/main/java/no/nav/tag/kontaktskjema/Kontaktskjema.java
@@ -1,12 +1,17 @@
 package no.nav.tag.kontaktskjema;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 
 @Data
+@Accessors
 public class Kontaktskjema {
     @Id
     private Integer id;
+    private String melding;
     private String fylke;
     private String kommune;
     private String bedriftsnavn;
@@ -14,4 +19,76 @@ public class Kontaktskjema {
     private String etternavn;
     private String epost;
     private String telefonnr;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMelding() {
+        return melding;
+    }
+
+    public void setMelding(String melding) {
+        this.melding = melding;
+    }
+
+    public String getFylke() {
+        return fylke;
+    }
+
+    public void setFylke(String fylke) {
+        this.fylke = fylke;
+    }
+
+    public String getKommune() {
+        return kommune;
+    }
+
+    public void setKommune(String kommune) {
+        this.kommune = kommune;
+    }
+
+    public String getBedriftsnavn() {
+        return bedriftsnavn;
+    }
+
+    public void setBedriftsnavn(String bedriftsnavn) {
+        this.bedriftsnavn = bedriftsnavn;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public String getTelefonnr() {
+        return telefonnr;
+    }
+
+    public void setTelefonnr(String telefonnr) {
+        this.telefonnr = telefonnr;
+    }
 }
--- src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaController.java
+++ src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaController.java
@@ -20,7 +20,24 @@ public class KontaktskjemaController {
     public ResponseEntity meldInteresse(
             @RequestBody Kontaktskjema kontaktskjema
     ) {
+        kontaktskjema.setMelding(genererMelding(kontaktskjema));
         repository.save(kontaktskjema);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private String genererMelding(Kontaktskjema kontaktskjema) {
+        return String.format("Emnefelt; Kontaktskjema Inkludering\n" +
+                "Arbeidsgiver har sendt henvendelse gjennom Kontaktskjema Inkludering;\n" +
+                "Navn: %s\n" +
+                "Nummer: %s %s\n" +
+                "E-post: %s\n" +
+                "Kommune: %s\n" +
+                "Kontakt arbeidsgiver for å   avklare hva henvendelsen gjelder.\n" +
+                "Minner om at arbeidsgiver skal kontaktes innen 48 timer.\n" +
+                "Husk å registrere henvendelsen som «Kontaktskjema Inkludering» i arena (ikke telefonkontakt)\n" +
+                "Når arbeidsgiver er kontaktet og henvendelsen registrert i Arena skal denne eposten slettes.\n",
+                kontaktskjema.getFornavn(), kontaktskjema.getEtternavn(), kontaktskjema.getTelefonnr(), kontaktskjema.getEpost(), kontaktskjema.getKommune()
+        );
+
     }
 }
