6727aa26f4d2be4d3a2c60d51e03c85bd282871a
andrmos <mossige.a@gmail.com>
2018-12-10 14:47:10 +0100

    TAG-138: Slå på deafult prometheus metrics

--- src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaApplication.java
+++ src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaApplication.java
@@ -1,5 +1,6 @@
 package no.nav.tag.kontaktskjema;
 
+import io.prometheus.client.hotspot.DefaultExports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KontaktskjemaApplication {
 
 	public static void main(String[] args) {
+		DefaultExports.initialize();
 		SpringApplication.run(KontaktskjemaApplication.class, args);
 	}
-
 }
