3190c589672edb98078ccd8a54b17acec9f90166
andrmos <mossige.a@gmail.com>
2018-12-11 12:12:28 +0100

    TAG-138: Fjern unødvendig metrikkonfigurasjon

--- src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaApplication.java
+++ src/main/java/no/nav/tag/kontaktskjema/KontaktskjemaApplication.java
@@ -1,6 +1,5 @@
 package no.nav.tag.kontaktskjema;
 
-import io.prometheus.client.hotspot.DefaultExports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KontaktskjemaApplication {
 
 	public static void main(String[] args) {
-		DefaultExports.initialize();
 		SpringApplication.run(KontaktskjemaApplication.class, args);
 	}
 }
