92143fc9f914bf23256ea174e1574b78741e8d2c
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-12-12 13:41:03 +0100

    TAG-102 feilende migrering

--- src/main/resources/db/migration/V2__legg-til-kommunenr.sql
+++ src/main/resources/db/migration/V2__legg-til-kommunenr.sql
@@ -1,3 +1,3 @@
 alter table KONTAKTSKJEMA add KOMMUNENR varchar(4);
 update KONTAKTSKJEMA set KOMMUNENR='0000';
-alter table KONTAKTSKJEMA add KOMMUNENR varchar(4) not null;
+alter table KONTAKTSKJEMA modify KOMMUNENR varchar(4) not null;
