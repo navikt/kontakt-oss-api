8ee21b96230e1b1e6c88cb3a7bcbd839b7081e2b
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-12-12 15:48:59 +0100

    TAG-102 Fremdeles feil sql

--- src/main/resources/db/migration/V2__legg-til-kommunenr.sql
+++ src/main/resources/db/migration/V2__legg-til-kommunenr.sql
@@ -1,3 +1,3 @@
 alter table KONTAKTSKJEMA add KOMMUNENR varchar(4);
 update KONTAKTSKJEMA set KOMMUNENR='0000';
-alter table KONTAKTSKJEMA alter column KOMMUNENR varchar(4) not null;
+alter table KONTAKTSKJEMA alter column KOMMUNENR set not null;
