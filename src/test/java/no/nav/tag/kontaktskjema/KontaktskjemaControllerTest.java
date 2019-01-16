99d73664b149a48c00042fc0c92555d2c8256a73
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-01-16 16:33:45 +0100

    Legg til test av innsendingsbegrensninger

--- src/test/java/no/nav/tag/kontaktskjema/KontaktskjemaControllerTest.java
+++ src/test/java/no/nav/tag/kontaktskjema/KontaktskjemaControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static no.nav.tag.kontaktskjema.TestData.lagKontaktskjema;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,5 +24,15 @@ public class KontaktskjemaControllerTest {
         Kontaktskjema kontaktskjema = lagKontaktskjema();
         kontaktskjema.setId(52);
         kontaktskjemaController.meldInteresse(kontaktskjema);
+    }
+
+    @Test
+    public void skalReturnere429VedForMangeInnsendinger() {
+        for (int i=0; i<10; i++) {
+            kontaktskjemaController.meldInteresse(lagKontaktskjema());
+        }
+
+        ResponseEntity result = kontaktskjemaController.meldInteresse(lagKontaktskjema());
+        assertThat(result.getStatusCode(), is(HttpStatus.TOO_MANY_REQUESTS));
     }
 }
