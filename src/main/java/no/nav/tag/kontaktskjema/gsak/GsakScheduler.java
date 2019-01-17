e071ef526dc787ce853570a5570fcbacf5b89eb4
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-01-17 11:33:08 +0100

    TAG-232 Oppretter shedlock-tabell og enkel tidsstyrt jobb

---
+++ src/main/java/no/nav/tag/kontaktskjema/gsak/GsakScheduler.java
@@ -0,0 +1,22 @@
+package no.nav.tag.kontaktskjema.gsak;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.SchedulerLock;
+
+@Slf4j
+@Component
+public class GsakScheduler {
+
+    private static final String ONE_MIN = "PT14M";
+
+    private static final String THIRTY_SECONDS = "PT30S";
+
+    @Scheduled(cron = "0 * * * * ?")
+    @SchedulerLock(name = "navn", lockAtMostForString = ONE_MIN, lockAtLeastForString = THIRTY_SECONDS)
+    public void scheduledBehandleSkjemaer() {
+        log.info("Kjører jobb for gsak");
+    }
+}
