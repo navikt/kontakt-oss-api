ce2211d9a6a8b28f32e9572d9b52002c511fb462
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-01-17 13:45:04 +0100

    Fjerner eksplisitt databasedriver

--- src/main/java/no/nav/tag/kontaktskjema/PostgresDataSourceConfiguration.java
+++ src/main/java/no/nav/tag/kontaktskjema/PostgresDataSourceConfiguration.java
@@ -21,7 +21,6 @@ public class PostgresDataSourceConfiguration {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
