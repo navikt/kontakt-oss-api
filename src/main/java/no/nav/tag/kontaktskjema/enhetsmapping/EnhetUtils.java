05ec1a33dd570c41115a188685bb2a483cc7747b
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-01-25 14:59:43 +0100

    TAG-234 Legg inn hele json-fil med enhetsmap

--- src/main/java/no/nav/tag/kontaktskjema/enhetsmapping/EnhetUtils.java
+++ src/main/java/no/nav/tag/kontaktskjema/enhetsmapping/EnhetUtils.java
@@ -1,4 +1,4 @@
-package no.nav.tag.kontaktskjema;
+package no.nav.tag.kontaktskjema.enhetsmapping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
