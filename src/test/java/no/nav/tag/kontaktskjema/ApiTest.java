6da2ae9c7dea3555a06f03fca1216762f9cc0fd6
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-01-28 11:13:18 +0100

    Fjerner litt duplisering

--- src/test/java/no/nav/tag/kontaktskjema/ApiTest.java
+++ src/test/java/no/nav/tag/kontaktskjema/ApiTest.java
@@ -1,5 +1,7 @@
 package no.nav.tag.kontaktskjema;
 
+import static java.net.http.HttpClient.newBuilder;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,7 +10,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,52 +21,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public class ApiTest {
 
+    private static final String OK_KONTAKTSKJEMA_JSON = "{ " +
+        "\"bedriftsnavn\": \"testbedrift\"," +
+        "\"epost\": \"test@testesen.no\"," +
+        "\"etternavn\": \"testesen\"," +
+        "\"fornavn\": \"test\"," +
+        "\"fylke\": \"agder\"," +
+        "\"kommune\": \"Audnedal\"," +
+        "\"kommunenr\": \"1027\"," +
+        "\"telefonnr\": \"1234\"" +
+        "}";
+
     @Test
     public void postKontaktskjema_OK() throws Exception {
 
-        String body = "{ " +
-            "\"bedriftsnavn\": \"testbedrift\"," +
-            "\"epost\": \"test@testesen.no\"," +
-            "\"etternavn\": \"testesen\"," +
-            "\"fornavn\": \"test\"," +
-            "\"fylke\": \"agder\"," +
-            "\"kommune\": \"Audnedal\"," +
-            "\"kommunenr\": \"1027\"," +
-            "\"telefonnr\": \"1234\"" +
-            "}";
-
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/kontaktskjema/meldInteresse"))
-                .header("Accept", "*/*")
-                .header("Accept-Encoding", "gzip, deflate, br")
-                .header("Accept-Language", "nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5")
-                .header("Cache-Control", "no-cache")
-                .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(body))
-                .build();
-
-        HttpResponse<?> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse<?> response = newBuilder().build().send(createRequest(OK_KONTAKTSKJEMA_JSON), ofString());
         assertThat(response.statusCode(), is(200));
         assertThat(response.body(), is("\"OK\""));
     }
 
-    @Test
-    public void postKontaktskjema_feil_kommunenr() throws Exception {
-
-        String body = "{ " +
-            "\"bedriftsnavn\": \"testbedrift\"," +
-            "\"epost\": \"test@testesen.no\"," +
-            "\"etternavn\": \"testesen\"," +
-            "\"fornavn\": \"test\"," +
-            "\"fylke\": \"agder\"," +
-            "\"kommune\": \"Audnedal\"," +
-            "\"kommunenr\": \"kommunenr\"," +
-            "\"telefonnr\": \"1234\"" +
-            "}";
-
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
+    private HttpRequest createRequest(String body) {
+        return HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080/kontaktskjema/meldInteresse"))
                 .header("Accept", "*/*")
                 .header("Accept-Encoding", "gzip, deflate, br")
@@ -74,10 +50,16 @@ public class ApiTest {
                 .header("Content-Type", "application/json")
                 .POST(BodyPublishers.ofString(body))
                 .build();
+    }
+    
+    @Test
+    public void postKontaktskjema_feil_kommunenr() throws Exception {
 
-        HttpResponse<?> response = client.send(request, BodyHandlers.ofString());
+        String bodyMedForLangtKommunenr = OK_KONTAKTSKJEMA_JSON.replaceFirst("1027", "10127");
+
+        HttpResponse<?> response = HttpClient.newBuilder().build().send(createRequest(bodyMedForLangtKommunenr), ofString());
         assertThat(response.statusCode(), is(500));
         assertThat(response.body(), is(""));
     }
-    
+
 }
