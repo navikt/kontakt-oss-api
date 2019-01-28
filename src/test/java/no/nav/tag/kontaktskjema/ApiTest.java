43dcf2669bf4ce1039788dffcf3e6d2ecaf21827
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-01-28 12:24:50 +0100

    Bruker RANDOM_PORT i stedet for DEFINED_PORT

--- src/test/java/no/nav/tag/kontaktskjema/ApiTest.java
+++ src/test/java/no/nav/tag/kontaktskjema/ApiTest.java
@@ -15,11 +15,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class ApiTest {
+
+    @LocalServerPort
+    private String port;
 
     private static final String OK_KONTAKTSKJEMA_JSON = "{ " +
         "\"bedriftsnavn\": \"testbedrift\"," +
@@ -42,7 +46,7 @@ public class ApiTest {
 
     private HttpRequest createRequest(String body) {
         return HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/kontaktskjema/meldInteresse"))
+                .uri(URI.create("http://localhost:" + port + "/kontaktskjema/meldInteresse"))
                 .header("Accept", "*/*")
                 .header("Accept-Encoding", "gzip, deflate, br")
                 .header("Accept-Language", "nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5")
