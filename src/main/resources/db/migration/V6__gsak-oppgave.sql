e5818c0cc415b2d9f1a89d682c4d005d4523a078
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-01-30 09:53:33 +0100

    TAG-237 number er ikke gyldig i postgres

--- src/main/resources/db/migration/V6__gsak-oppgave.sql
+++ src/main/resources/db/migration/V6__gsak-oppgave.sql
@@ -1,7 +1,7 @@
 create table GSAK_OPPGAVE (
     id serial primary key,
-    kontaktskjema_id number not null ,
-    gsak_id number,
+    kontaktskjema_id numeric not null,
+    gsak_id numeric,
     status varchar(255),
     opprettet timestamp(6) default current_timestamp
 );
