eb403ef3d2a746b029b2b614983354f3cc825fa5
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-04 16:13:49 +0100

    TAG-233 testfix

--- src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveForSkjemaTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveForSkjemaTest.java
@@ -2,11 +2,12 @@ package no.nav.tag.kontakt.oss.gsak;
 
 import no.nav.tag.kontakt.oss.DateProvider;
 import no.nav.tag.kontakt.oss.Kontaktskjema;
+import no.nav.tag.kontakt.oss.enhetsmapping.EnhetUtils;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
 
-import static no.nav.tag.kontakt.oss.gsak.GsakOppgave.OppgaveStatus.DISABLED;
+import static no.nav.tag.kontakt.oss.gsak.GsakOppgave.OppgaveStatus.OK;
 import static org.mockito.Mockito.*;
 
 public class GsakOppgaveForSkjemaTest {
@@ -20,13 +21,14 @@ public class GsakOppgaveForSkjemaTest {
         GsakOppgaveForSkjema gsakOppgaveForSkjema = new GsakOppgaveForSkjema(
                 oppgaveRepository,
                 dateProvider,
-                mock(GsakKlient.class)
+                mock(GsakKlient.class),
+                mock(EnhetUtils.class)
         );
 
         Kontaktskjema lagKontaktskjema = Kontaktskjema.builder().id(5).build();
         gsakOppgaveForSkjema.opprettOppgaveOgLagreStatus(lagKontaktskjema);
 
-        verify(oppgaveRepository).save(eq(GsakOppgave.builder().kontaktskjemaId(5).status(DISABLED).opprettet(dateProvider.now()).build()));
+        verify(oppgaveRepository).save(eq(GsakOppgave.builder().gsakId(0).kontaktskjemaId(5).status(OK).opprettet(dateProvider.now()).build()));
 
     }
 }
