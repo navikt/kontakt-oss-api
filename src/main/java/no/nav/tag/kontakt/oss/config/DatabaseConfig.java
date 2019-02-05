14be600a6edf25ba2d3618f399be8c5bf9d461f9
André Mossige <mossige.a@gmail.com>
2019-02-05 13:44:16 +0100

    TAG-273 Forenkle databaseoppsett

--- src/main/java/no/nav/tag/kontakt/oss/config/DatabaseConfig.java
+++ src/main/java/no/nav/tag/kontakt/oss/config/DatabaseConfig.java
@@ -1,14 +1,14 @@
 package no.nav.tag.kontakt.oss.config;
 
-
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.SneakyThrows;
 import no.nav.tag.kontakt.oss.vault.HikariCPVaultUtil;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
@@ -24,13 +24,7 @@ public class DatabaseConfig {
     @Value("${NAIS_CLUSTER_NAME}")
     private String miljo;
 
-    @Bean("adminDataSource")
-    public DataSource adminDataSource() {
-        return dataSource("admin");
-    }
-
     @Bean
-    @Primary
     public DataSource userDataSource() {
         return dataSource("user");
     }
@@ -44,10 +38,18 @@ public class DatabaseConfig {
         String mountPath = miljo.equals("prod-fss")
                 ? "postgresql/prod-fss"
                 : "postgresql/preprod-fss";
-        return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, mountPath, dbRole(miljo, user));
+        return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, mountPath, dbRole(user));
     }
 
-    static String dbRole(String miljo, String role) {
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> Flyway.configure().dataSource(dataSource("admin"))
+                .initSql(String.format("SET ROLE \"%s\"", dbRole("admin")))
+                .load()
+                .migrate();
+    }
+
+    private String dbRole(String role) {
         String databaseNavnProd = "kontakt-oss-prod";
         String databaseNavnPreprod = "kontakt-oss-preprod";
         return miljo.equals("prod-fss")
