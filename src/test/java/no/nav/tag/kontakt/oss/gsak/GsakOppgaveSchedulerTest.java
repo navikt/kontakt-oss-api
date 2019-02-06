0bad530ceac95e8df00457a35eb520cf300e391f
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-06 16:04:49 +0100

    TAG-233 Fiks feilende test

--- src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveSchedulerTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveSchedulerTest.java
@@ -4,21 +4,29 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import no.nav.tag.kontakt.oss.KontaktskjemaRepository;
 import org.junit.Test;
 
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockingTaskExecutor;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class GsakOppgaveSchedulerTest {
+
+    @Mock KontaktskjemaRepository kontaktskjemaRepository;
+    @Mock GsakOppgaveService oppgaveForSkjema;
+    @Mock LockingTaskExecutor taskExecutor;
 
     @Test
     public void skalSjekkeAtSceduledMetodeBrukerShedlock() {
-        GsakOppgaveScheduler gsakScheduler = new GsakOppgaveScheduler();
-        gsakScheduler.taskExecutor = mock(LockingTaskExecutor.class);
-        
+        GsakOppgaveScheduler gsakScheduler = new GsakOppgaveScheduler(kontaktskjemaRepository, oppgaveForSkjema, taskExecutor);
+
         gsakScheduler.scheduledOpprettOppgaveForSkjemaer();
         
-        verify(gsakScheduler.taskExecutor).executeWithLock(any(Runnable.class), any(LockConfiguration.class));
+        verify(taskExecutor).executeWithLock(any(Runnable.class), any(LockConfiguration.class));
     }
 
 }
