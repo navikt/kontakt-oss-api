e5b468637b1083b1eaddc373b4df5be7ba5db857
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-06 12:54:44 +0100

    TAG-233 Legg til ordentlig testdata i testcontroller

--- src/main/java/no/nav/tag/kontakt/oss/gsak/GsakController.java
+++ src/main/java/no/nav/tag/kontakt/oss/gsak/GsakController.java
@@ -1,6 +1,7 @@
 package no.nav.tag.kontakt.oss.gsak;
 
 import no.nav.tag.kontakt.oss.DateProvider;
+import no.nav.tag.kontakt.oss.Kontaktskjema;
 import no.nav.tag.kontakt.oss.gsak.integrasjon.GsakKlient;
 import no.nav.tag.kontakt.oss.gsak.integrasjon.GsakRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 // TODO TAG-233 Skal fjernes, er bare for å teste gsak-apiet direkte
 @RestController
@@ -24,9 +26,38 @@ public class GsakController {
     @GetMapping(value = "${controller.basepath}/gsak")
     public ResponseEntity gsak() {
         LocalDate aktivDato = new DateProvider().now().toLocalDate();
+        Kontaktskjema kontaktskjema = new Kontaktskjema(
+                null,
+                LocalDateTime.now(),
+                "nordland",
+                "Bodø",
+                "0011",
+                "Flesk og Fisk AS",
+                "123456789",
+                "Ola",
+                "Nordmann",
+                "ola.nordmann@fleskOgFisk.no",
+                "01234567",
+                "Rekruttering"
+        );
+
+        String beskrivelse = String.format(
+                "Arbeidsgiver har sendt henvendelse gjennom Kontaktskjema; \n" +
+                        "Navn: %s \n" +
+                        "Nummer: %s \n" +
+                        "E-post: %s \n" +
+                        "Kommune: %s (kommunenr: %s) \n" +
+                        "Kontakt arbeidsgiver for å avklare hva henvendelsen gjelder. Husk å registrere henvendelsen som aktivitetstype «Kontaktskjema» i Arena.",
+                kontaktskjema.getFornavn() + " " + kontaktskjema.getEtternavn(),
+                kontaktskjema.getTelefonnr(),
+                kontaktskjema.getEpost(),
+                kontaktskjema.getKommune(),
+                kontaktskjema.getKommunenr()
+        );
+
         GsakRequest innsending = new GsakRequest(
                 "0315",
-                "blabla beskrivelse",
+                beskrivelse,
                 "ARBD",
                 "OPA",
                 "VURD_HENV",
