e237aaa856f1ab92a359d5e75adce918fe86be5c
André Mossige <mossige.a@gmail.com>
2019-02-12 12:27:23 +0100

    TAG-286 Slå av CORS sjekk i backend

--- src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
+++ src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
@@ -4,14 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@CrossOrigin(origins = {"https://tjenester.nav.no", "https://tjenester-q1.nav.no", "https://tjenester-t1.nav.no"})
 @RestController
 @Slf4j
 public class KontaktskjemaController {
