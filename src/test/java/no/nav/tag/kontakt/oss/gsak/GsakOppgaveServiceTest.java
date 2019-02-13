24379a5ff9e01f6fd1d02da096edfbf5efddbcd5
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-13 10:09:51 +0100

    TAG-289 Legg til feature toggle for gsak!

--- src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveServiceTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/gsak/GsakOppgaveServiceTest.java
@@ -2,12 +2,14 @@ package no.nav.tag.kontakt.oss.gsak;
 
 import no.nav.tag.kontakt.oss.DateProvider;
 import no.nav.tag.kontakt.oss.Kontaktskjema;
+import no.nav.tag.kontakt.oss.featureToggles.FeatureToggles;
 import no.nav.tag.kontakt.oss.navenhetsmapping.NavEnhetUtils;
 import no.nav.tag.kontakt.oss.gsak.integrasjon.GsakKlient;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
 
+import static no.nav.tag.kontakt.oss.gsak.GsakOppgave.OppgaveStatus.DISABLED;
 import static no.nav.tag.kontakt.oss.gsak.GsakOppgave.OppgaveStatus.OK;
 import static org.mockito.Mockito.*;
 
@@ -23,7 +25,8 @@ public class GsakOppgaveServiceTest {
                 oppgaveRepository,
                 dateProvider,
                 mock(GsakKlient.class),
-                mock(NavEnhetUtils.class)
+                mock(NavEnhetUtils.class),
+                altErEnabled()
         );
 
         Kontaktskjema kontaktskjema = Kontaktskjema.builder().id(5).build();
@@ -31,5 +34,30 @@ public class GsakOppgaveServiceTest {
 
         verify(oppgaveRepository).save(eq(GsakOppgave.builder().gsakId(0).kontaktskjemaId(5).status(OK).opprettet(dateProvider.now()).build()));
 
+    }
+
+    @Test
+    public void skalReturnereDisabledHvisGsakToggleErAv() {
+        GsakOppgaveRepository oppgaveRepository = mock(GsakOppgaveRepository.class);
+        FeatureToggles featureToggles = mock(FeatureToggles.class);
+        when(featureToggles.isEnabled(eq("gsak"))).thenReturn(false);
+
+        GsakOppgaveService gsakOppgaveForSkjema = new GsakOppgaveService(
+                oppgaveRepository,
+                mock(DateProvider.class),
+                mock(GsakKlient.class),
+                mock(NavEnhetUtils.class),
+                featureToggles
+        );
+
+        gsakOppgaveForSkjema.opprettOppgaveOgLagreStatus(Kontaktskjema.builder().build());
+        verify(oppgaveRepository).save(eq(GsakOppgave.builder().gsakId(null).status(DISABLED).build()));
+
+    }
+
+    private FeatureToggles altErEnabled() {
+        FeatureToggles featureToggles = mock(FeatureToggles.class);
+        when(featureToggles.isEnabled(anyString())).thenReturn(true);
+        return featureToggles;
     }
 }
