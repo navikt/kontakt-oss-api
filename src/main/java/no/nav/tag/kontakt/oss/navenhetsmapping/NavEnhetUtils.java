3c9c480611cb09c14493638b8303e9e2ea177be3
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-19 16:42:38 +0100

    TAG-297 Legg til enkel norg-klient

--- src/main/java/no/nav/tag/kontakt/oss/navenhetsmapping/NavEnhetUtils.java
+++ src/main/java/no/nav/tag/kontakt/oss/navenhetsmapping/NavEnhetUtils.java
@@ -1,5 +1,6 @@
 package no.nav.tag.kontakt.oss.navenhetsmapping;
 
+import no.nav.tag.kontakt.oss.norg.NorgKlient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,13 @@ import java.util.Map;
 @Component
 public class NavEnhetUtils {
     private final Map<String, String> enheter;
+    private final NorgKlient norgKlient;
 
     @Autowired
-    public NavEnhetUtils(Map<String, String> enheter) {
+    public NavEnhetUtils(Map<String, String> enheter, NorgKlient norgKlient) {
         this.enheter = enheter;
+        this.norgKlient = norgKlient;
+        norgKlient.hentGeografiFraNorg();
     }
 
     public String mapFraKommunenrTilEnhetsnr(String kommunenr) {
