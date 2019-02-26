41ac376cf5406da92006d917e1274d1b1f29ec57
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-26 15:30:52 +0100

    TAG-298 Legg til foreløpig endepunkt

--- src/main/java/no/nav/tag/kontakt/oss/fylkesinndelingMedNavEnheter/GeografiController.java
+++ src/main/java/no/nav/tag/kontakt/oss/fylkesinndelingMedNavEnheter/GeografiController.java
@@ -1,5 +1,7 @@
 package no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter;
 
+import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.integrasjon.NorgKlient;
+import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.integrasjon.NorgOrganisering;
 import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.integrasjon.NorgService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +12,25 @@ import java.util.List;
 public class GeografiController {
 
     private final NorgService norgService;
+    private final NorgKlient norgKlient;
 
-    public GeografiController(NorgService norgService) {
+    public GeografiController(NorgService norgService, NorgKlient norgKlient) {
         this.norgService = norgService;
+        this.norgKlient = norgKlient;
     }
 
     @GetMapping(value = "${controller.basepath}/geografi")
-    public Object geografi() {
+    public FylkesinndelingMedNavEnheter geografi() {
         List<KommuneEllerBydel> kommunerOgBydeler = norgService.hentListeOverAlleKommunerOgBydeler();
         return new FylkesinndelingMedNavEnheter(
                 norgService.hentMapFraNavenhetTilFylkesenhet(),
                 norgService.hentMapFraKommuneEllerBydelTilNavenhet(kommunerOgBydeler),
                 kommunerOgBydeler
         );
+    }
+
+    @GetMapping(value = "${controller.basepath}/geografi")
+    public List<NorgOrganisering> organisering() {
+        return norgKlient.hentOrganiseringFraNorg();
     }
 }
