6c6e7dfc0ea8da4789db4703f656dda939cfa4fd
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-26 09:43:37 +0100

    TAG-307 Legg til toString slik at man kan se hvilken kommune/bydel det er snakk om i loggen.

--- src/main/java/no/nav/tag/kontakt/oss/geografi/integrasjon/Bydel.java
+++ src/main/java/no/nav/tag/kontakt/oss/geografi/integrasjon/Bydel.java
@@ -3,10 +3,12 @@ package no.nav.tag.kontakt.oss.geografi.integrasjon;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @EqualsAndHashCode
 @AllArgsConstructor
 @Getter
+@ToString
 public class Bydel implements KommuneEllerBydel {
     private String nummer;
     private String navn;
