1d040628e400dc6f5b8551bb07f351cf411caa88
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-02-28 12:38:33 +0100

    TAG-311 Legg til shedlock på norg-henting

--- src/main/java/no/nav/tag/kontakt/oss/fylkesinndelingMedNavEnheter/FylkesinndelingController.java
+++ src/main/java/no/nav/tag/kontakt/oss/fylkesinndelingMedNavEnheter/FylkesinndelingController.java
@@ -1,30 +1,19 @@
 package no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter;
 
-import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.integrasjon.NorgService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class FylkesinndelingController {
 
-    private final NorgService norgService;
     private final FylkesinndelingRepository fylkesinndelingRepository;
 
-    public FylkesinndelingController(NorgService norgService, FylkesinndelingRepository fylkesinndelingRepository) {
-        this.norgService = norgService;
+    public FylkesinndelingController(FylkesinndelingRepository fylkesinndelingRepository) {
         this.fylkesinndelingRepository = fylkesinndelingRepository;
     }
 
     @GetMapping(value = "${controller.basepath}/fylkerOgKommuner")
     public FylkesinndelingMedNavEnheter hentFylkerOgKommuner() {
-        // TODO TAG-311 Før dette endepunktet kan tas i bruk i frontend, må NORG-kallene caches.
-        List<KommuneEllerBydel> kommunerOgBydeler = norgService.hentListeOverAlleKommunerOgBydeler();
-        return new FylkesinndelingMedNavEnheter(
-                norgService.hentMapFraNavenhetTilFylkesenhet(),
-                norgService.hentMapFraKommuneEllerBydelTilNavenhet(kommunerOgBydeler),
-                kommunerOgBydeler
-        );
+        return fylkesinndelingRepository.hentFylkesinndeling();
     }
 }
