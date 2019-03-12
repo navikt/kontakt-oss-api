0f8a9fe7cb48b836befd7d2e8f1def98dd2efe41
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-03-12 12:25:05 +0100

    TAG-346 Forenkle mock-serveren

--- src/main/java/no/nav/tag/kontakt/oss/mock/MockServer.java
+++ src/main/java/no/nav/tag/kontakt/oss/mock/MockServer.java
@@ -32,28 +32,32 @@ public class MockServer {
     @Autowired
     MockServer(
             @Value("${norg.url}") String norgUrl,
+            @Value("${kodeverk.url}") String kodeverkUrl,
             @Value("${mock.port}") Integer port
     ) {
         log.info("Starter mock-server");
 
         this.server =  new WireMockServer(port);
         String norgPath = new URL(norgUrl).getPath();
+        String kodeverkPath = new URL(kodeverkUrl).getPath();
 
-        mockNorgOrganisering(norgPath);
         mockNorgsMappingFraGeografiTilNavEnhet(norgPath);
-
-        String kodeverkPath = "/kodeverk";
-        mockKall(kodeverkPath + "/kodeverk/Kommuner/koder/betydninger", "kommuner.json");
-        mockKall(kodeverkPath + "/kodeverk/Bydeler/koder/betydninger", "bydeler.json");
+        mockKallFraFil(norgPath + "/enhet/kontaktinformasjon/organisering/AKTIV", "norgOrganisering.json");
+        mockKallFraFil(kodeverkPath + "/kodeverk/Kommuner/koder/betydninger", "kommuner.json");
+        mockKallFraFil(kodeverkPath + "/kodeverk/Bydeler/koder/betydninger", "bydeler.json");
 
         server.start();
     }
 
-    private void mockKall(String path, String filnavn) {
+    private void mockKallFraFil(String path, String filnavn) {
+        mockKall(path, lesFilSomString(filnavn));
+    }
+
+    private void mockKall(String path, String body) {
         server.stubFor(
                 WireMock.get(WireMock.urlPathEqualTo(path)).willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
-                        .withBody(hentStringFraFil(filnavn))
+                        .withBody(body)
                 )
         );
     }
@@ -61,7 +65,7 @@ public class MockServer {
     @SneakyThrows
     private void mockNorgsMappingFraGeografiTilNavEnhet(String norgPath) {
         Map<String, NavEnhet> fraKommuneEllerBydelTilNavEnhet = objectMapper.readValue(
-                hentStringFraFil("mapFraKommuneTilNavEnhet.json"),
+                lesFilSomString("mapFraKommuneTilNavEnhet.json"),
                 new TypeReference<Map<String, NavEnhet>>() {}
         );
 
@@ -71,27 +75,12 @@ public class MockServer {
         for (String kommuneNrEllerBydelNr : fraKommuneEllerBydelTilNavEnhet.keySet()) {
             navEnhet = fraKommuneEllerBydelTilNavEnhet.get(kommuneNrEllerBydelNr);
             navEnhetJson = objectMapper.writeValueAsString(navEnhet);
-            server.stubFor(
-                    WireMock.get(WireMock.urlPathEqualTo(norgPath + "/enhet/navkontor/" + kommuneNrEllerBydelNr)).willReturn(WireMock.aResponse()
-                            .withStatus(HttpStatus.OK.value())
-                            .withBody(navEnhetJson)
-                    )
-            );
+            mockKall(norgPath + "/enhet/navkontor/" + kommuneNrEllerBydelNr, navEnhetJson);
         }
     }
 
-    private void mockNorgOrganisering(String norgPath) {
-        server.stubFor(
-                WireMock.get(WireMock.urlPathEqualTo(norgPath + "/enhet/kontaktinformasjon/organisering/AKTIV")).willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withBody(hentStringFraFil("norgOrganisering.json"))
-                )
-        );
-
-    }
-
     @SneakyThrows
-    private String hentStringFraFil(String filnavn) {
+    private String lesFilSomString(String filnavn) {
         return IOUtils.toString(this.getClass().getClassLoader().getResourceAsStream("mock/" + filnavn), UTF_8);
     }
 }
