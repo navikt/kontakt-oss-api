3bc473d96ac14b3258ebdb5940fe364caad5dcad
André Mossige <mossige.a@gmail.com>
2019-03-25 11:53:38 +0100

    TAG-419 Bytt ut spring-cloud-vault med vault-jdbc

--- src/main/java/no/nav/tag/kontakt/oss/config/DatabaseConfig.java
+++ src/main/java/no/nav/tag/kontakt/oss/config/DatabaseConfig.java
@@ -1,7 +1,10 @@
 package no.nav.tag.kontakt.oss.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.SneakyThrows;
+import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
@@ -13,15 +16,44 @@ import javax.sql.DataSource;
 @Configuration
 @Profile({"preprod", "prod"})
 public class DatabaseConfig {
+
+    @Value("${SPRING_DATASOURCE_URL}")
+    private String databaseUrl;
+
+    @Value("${NAIS_CLUSTER_NAME}")
+    private String miljo;
+
     @Bean
-    public FlywayMigrationStrategy flywayMigrationStrategy(
-            @Autowired DataSource dataSource,
-            @Value("${spring.cloud.vault.database.role}") String adminRolle
-    ) {
+    public DataSource userDataSource() {
+        return dataSource("user");
+    }
+
+    @SneakyThrows
+    private HikariDataSource dataSource(String user) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(databaseUrl);
+        config.setMaximumPoolSize(3);
+        config.setMinimumIdle(1);
+        String mountPath = miljo.equals("prod-fss")
+                ? "postgresql/prod-fss"
+                : "postgresql/preprod-fss";
+        return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, mountPath, dbRole(user));
+    }
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
         return flyway -> Flyway.configure()
-                .dataSource(dataSource)
-                .initSql(String.format("SET ROLE \"%s\"", adminRolle))
+                .dataSource(dataSource("admin"))
+                .initSql(String.format("SET ROLE \"%s\"", dbRole("admin")))
                 .load()
                 .migrate();
+    }
+
+    private String dbRole(String role) {
+        String databaseNavnProd = "kontakt-oss-prod";
+        String databaseNavnPreprod = "kontakt-oss-preprod";
+        return miljo.equals("prod-fss")
+                ? String.join("-", databaseNavnProd, role)
+                : String.join("-", databaseNavnPreprod, role);
     }
 }
