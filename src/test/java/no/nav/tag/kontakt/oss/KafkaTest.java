0aecf2d633dfcbb250040f08d688652cc94734bd
André Mossige <mossige.a@gmail.com>
2019-05-21 13:46:51 +0200

    Sjekk at json inneholder riktige felter

--- src/test/java/no/nav/tag/kontakt/oss/KafkaTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/KafkaTest.java
@@ -1,10 +1,15 @@
 package no.nav.tag.kontakt.oss;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.tag.kontakt.oss.events.BesvarelseMottatt;
+import no.nav.tag.kontakt.oss.kafka.KontaktskjemaForKafka;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -21,10 +26,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import static no.nav.tag.kontakt.oss.TestData.kontaktskjema;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,10 +41,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext
 public class KafkaTest {
 
+    static final String TOPIC = "aapen-tag-kontaktskjemaMottatt";
+
     @Autowired
     private ApplicationEventPublisher eventPublisher;
-
-    static final String TOPIC = "aapen-tag-kontaktskjemaMottatt";
 
     @ClassRule
     public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, 1, TOPIC);
@@ -55,15 +63,25 @@ public class KafkaTest {
     }
 
     @Test
-    public void besvarelseMottatt__skal_sende_kontaktskjema_på_kafka_topic() {
+    public void besvarelseMottatt__skal_sende_kontaktskjema_på_kafka_topic_med_riktige_felter() throws JSONException {
         Kontaktskjema kontaktskjema = kontaktskjema();
         kontaktskjema.setId(1);
         eventPublisher.publishEvent(new BesvarelseMottatt(true, kontaktskjema));
 
-        ConsumerRecords<String, String> replies = KafkaTestUtils.getRecords(consumer);
+        ConsumerRecord<String, String> record = KafkaTestUtils.getSingleRecord(consumer, TOPIC);
 
-        assertThat(replies.count()).isGreaterThanOrEqualTo(1);
+        JSONObject json = new JSONObject(record.value());
+        assertThat(json.getInt("id")).isEqualTo(kontaktskjema.getId());
 
-        // Sjekk at konsumert string inneholder riktige verdier. ex. bedriftsnr ++
+        LocalDateTime opprettet = LocalDateTime.parse(json.getString("opprettet"));
+        assertThat(opprettet).isEqualToIgnoringNanos(kontaktskjema.getOpprettet());
+
+        assertThat(json.getString("fylke")).isEqualTo(kontaktskjema.getFylke());
+        assertThat(json.getString("kommune")).isEqualTo(kontaktskjema.getKommune());
+        assertThat(json.getString("kommunenr")).isEqualTo(kontaktskjema.getKommunenr());
+        assertThat(json.getString("bedriftsnavn")).isEqualTo(kontaktskjema.getBedriftsnavn());
+        assertThat(json.getString("orgnr")).isEqualTo(kontaktskjema.getOrgnr());
+        assertThat(json.getString("tema")).isEqualTo(kontaktskjema.getTema());
+        assertThat(json.getString("temaType")).isEqualTo(kontaktskjema.getTemaType().toString());
     }
 }
