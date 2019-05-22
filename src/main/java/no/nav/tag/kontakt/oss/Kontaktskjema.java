91853e26762a0b4c0538d1bf80111f019b4ff268
André Mossige <mossige.a@gmail.com>
2019-05-22 09:53:36 +0200

    Gjør om har snakket med representant felt til Boolean

--- src/main/java/no/nav/tag/kontakt/oss/Kontaktskjema.java
+++ src/main/java/no/nav/tag/kontakt/oss/Kontaktskjema.java
@@ -29,5 +29,5 @@ public class Kontaktskjema {
     private String telefonnr;
     private String tema;
     private TemaType temaType;
-    private boolean harSnakketMedAnsattrepresentant;
+    private Boolean harSnakketMedAnsattrepresentant;
 }
