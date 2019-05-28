1325039b5ebf99949ae7396ffc0a2b48c9305dd9
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-05-28 16:25:22 +0200

    Revert "TAG-527 Støtt flere feature toggles; trekk ut til service"
    This reverts commit 64cd74eb87a2d2d20efd2d5ab23b4fa6028e358c.

--- src/main/java/no/nav/tag/kontakt/oss/featureToggles/FeatureToggleController.java
+++ src/main/java/no/nav/tag/kontakt/oss/featureToggles/FeatureToggleController.java
@@ -1,32 +1,32 @@
 package no.nav.tag.kontakt.oss.featureToggles;
 
+import no.finn.unleash.Unleash;
+import no.finn.unleash.UnleashContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
 public class FeatureToggleController {
-    private final FeatureToggleService featureToggleService;
+    private final Unleash unleash;
     private final String UNLEASH_SESSION = "unleash-session";
 
     @Autowired
-    public FeatureToggleController(FeatureToggleService featureToggleService) {
-        this.featureToggleService = featureToggleService;
+    public FeatureToggleController(Unleash unleash) {
+        this.unleash = unleash;
     }
 
-    @GetMapping("/feature")
-    public ResponseEntity<Map<String, Boolean>> feature(
-            @RequestParam("feature") List<String> features,
+    @GetMapping("/feature/{feature}")
+    public ResponseEntity feature(
+            @PathVariable String feature,
             @CookieValue(name = UNLEASH_SESSION, required = false) String unleashSession,
             HttpServletResponse response
     ) {
@@ -37,8 +37,9 @@ public class FeatureToggleController {
             response.addCookie(new Cookie(UNLEASH_SESSION, sessionId));
         }
 
-        Map<String, Boolean> toggles = featureToggleService.hentFeatureToggles(features, sessionId);
-        return ResponseEntity.status(HttpStatus.OK).body(toggles);
+        UnleashContext unleashContext = UnleashContext.builder().sessionId(sessionId).build();
+        boolean isEnabled = unleash.isEnabled(feature, unleashContext);
 
+        return ResponseEntity.status(HttpStatus.OK).body(isEnabled);
     }
 }
