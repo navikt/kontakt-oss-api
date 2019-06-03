03929d419cef7190515167bd92104ab7ca168b0f
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-06-03 14:33:09 +0200

    TAG-545 Ikke bruk json-loggign

--- src/main/java/no/nav/tag/kontakt/oss/metrics/MetricsListeners.java
+++ src/main/java/no/nav/tag/kontakt/oss/metrics/MetricsListeners.java
@@ -1,8 +1,5 @@
 package no.nav.tag.kontakt.oss.metrics;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeCreator;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
@@ -20,8 +17,6 @@ import java.util.List;
 @Slf4j
 @Component
 public class MetricsListeners {
-
-    private final static ObjectMapper objectMapper = new ObjectMapper();
 
     private final MeterRegistry meterRegistry;
 
@@ -80,18 +75,16 @@ public class MetricsListeners {
     public void besvarelseMottatt(BesvarelseMottatt event) {
         Kontaktskjema kontaktskjema = event.getKontaktskjema();
 
-        ObjectNode json = objectMapper.createObjectNode();
-
-        json.put("event", "kontaktskjema.mottatt");
-        json.put("success", event.isSuksess());
-        json.put("fylke", kontaktskjema.getFylke());
-        json.put("kommunenr", kontaktskjema.getKommunenr());
-        json.put("kommune", kontaktskjema.getKommune());
-        json.put("orgnr", kontaktskjema.getOrgnr());
-        json.put("temaType", kontaktskjema.getTemaType().name());
-        json.put("harSnakketMedAnsattrepresentant", kontaktskjema.getHarSnakketMedAnsattrepresentant());
-
-        log.info(json.toString());
+        log.info(
+                "event=kontaktskjema.mottatt"
+                + "success=" + event.isSuksess()
+                + "fylke=" + kontaktskjema.getFylke()
+                + "kommunenr=" + kontaktskjema.getKommunenr()
+                + "kommune=" + kontaktskjema.getKommune()
+                + "orgnr=" + kontaktskjema.getOrgnr()
+                + "temaType=" + kontaktskjema.getTemaType()
+                + "harSnakketMedAnsattrepresentant=" + kontaktskjema.getHarSnakketMedAnsattrepresentant()
+        );
 
         // TODO: Fjern koden under når Kibana-boardet er oppe
         String counterName = event.isSuksess() ? KONTAKTSKJEMA_SUCCESS_COUNTER : KONTAKTSKJEMA_FAIL_COUNTER;
