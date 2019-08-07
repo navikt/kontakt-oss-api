b3304404e3ea732736f24bf65867684608c40a6c
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-08-07 13:39:44 +0200

    Valider gyldighet til kontaktskjema i controller

--- src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
+++ src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
@@ -1,13 +1,17 @@
 package no.nav.tag.kontakt.oss;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.regex.Pattern;
+
+@Slf4j
 @RestController
 public class KontaktskjemaController {
-
     private final KontaktskjemaService kontaktskjemaService;
 
     @Autowired
@@ -19,12 +23,48 @@ public class KontaktskjemaController {
 
     @PostMapping(value = "/meldInteresse")
     public ResponseEntity meldInteresse(
-            @RequestBody Kontaktskjema kontaktskjema
+            @Valid  @RequestBody Kontaktskjema kontaktskjema
     ) {
         if (kontaktskjemaService.harMottattForMangeInnsendinger()) {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
+
+        if (!erGyldigKontaktskjema(kontaktskjema)) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
         kontaktskjemaService.lagreKontaktskjema(kontaktskjema);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private boolean erGyldigKontaktskjema(Kontaktskjema kontaktskjema) {
+        String latin = "a-zA-Z \\-'";
+        String norsk = "æøåÆØÅ";
+        String sifre = "0-9";
+        String eposttegn = "\\.@+";
+        String aksenter = "ëÿüïöäéúíóáèùìòàêûîôâõãñËŸÜÏÖÄÉÚÍÓÁÈÙÌÒÀÊÛÎÔÂÕÃÑ";
+
+        String rausTekst = latin + norsk + sifre + aksenter;
+        String epost = latin + norsk + sifre + aksenter + eposttegn;
+
+        return erGyldigFelt(kontaktskjema.getBedriftsnavn(), rausTekst) &&
+                erGyldigFelt(kontaktskjema.getFornavn(), rausTekst) &&
+                erGyldigFelt(kontaktskjema.getEtternavn(), rausTekst) &&
+                erGyldigFelt(kontaktskjema.getFylke(), rausTekst) &&
+                erGyldigFelt(kontaktskjema.getKommune(), rausTekst) &&
+                erGyldigFelt(kontaktskjema.getOrgnr(), sifre + " ") &&
+                erGyldigFelt(kontaktskjema.getTelefonnr(), sifre + "+ ") &&
+                erGyldigFelt(kontaktskjema.getEpost(), epost);
+    }
+
+    private boolean erGyldigFelt(String felt, String skalBareInneholde) {
+        Pattern pattern = Pattern.compile("^[" + skalBareInneholde + "]*$");
+        if (pattern.matcher(felt).matches()) {
+            return true;
+        } else {
+            log.error("Skjemafelt \"" + felt + "\" må bare inneholde \"" + skalBareInneholde + "\"");
+            return false;
+        }
     }
 }
--- src/test/java/no/nav/tag/kontakt/oss/KontaktskjemaControllerTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/KontaktskjemaControllerTest.java
@@ -1,5 +1,11 @@
 package no.nav.tag.kontakt.oss;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +14,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
 import static no.nav.tag.kontakt.oss.testUtils.TestData.kontaktskjema;
+import static no.nav.tag.kontakt.oss.testUtils.TestData.kontaktskjemaBuilder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
@@ -42,5 +49,43 @@ public class KontaktskjemaControllerTest {
     @Test
     public void meldInteresse__skal_returnere_200_dersom_OK() {
         assertThat(kontaktskjemaController.meldInteresse(kontaktskjema()).getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    @SneakyThrows
+    public void meldInteresse__skal_returnere_hvis_kontaktskjema_er_gyldig() {
+        Kontaktskjema gyldigKontaktskjema = kontaktskjemaBuilder()
+                .bedriftsnavn("Årvõll Øks3sk4ft")
+                .epost("hei@årvoll.øks3-sk4ft.no")
+                .telefonnr("+47 99 99 99 99")
+                .orgnr("123 456 789")
+                .build();
+
+        assertThat(kontaktskjemaController.meldInteresse(gyldigKontaktskjema).getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    @SneakyThrows
+    public void meldInteresse__skal_returnere_400_ved_ugyldig_skjema() {
+        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
+                .bedriftsnavn("$jokoladefabrikken")
+                .build();
+
+        Kontaktskjema ugyldigKontaktskjema2 = kontaktskjemaBuilder()
+                .epost("hei@$jokoladefabrikken.no")
+                .build();
+
+        Kontaktskjema ugyldigKontaktskjema3 = kontaktskjemaBuilder()
+                .telefonnr("abcde")
+                .build();
+
+        Kontaktskjema ugyldigKontaktskjema4 = kontaktskjemaBuilder()
+                .orgnr("abcde")
+                .build();
+
+        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema).getStatusCode(), is(HttpStatus.BAD_REQUEST));
+        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema2).getStatusCode(), is(HttpStatus.BAD_REQUEST));
+        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema3).getStatusCode(), is(HttpStatus.BAD_REQUEST));
+        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema4).getStatusCode(), is(HttpStatus.BAD_REQUEST));
     }
 }
