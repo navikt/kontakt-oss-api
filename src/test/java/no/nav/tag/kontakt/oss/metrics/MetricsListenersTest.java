d2369222ccc2dae8fbd66dcca1e012ae6f77c9e7
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-08-08 09:01:08 +0200

    Fjerner bruk av DISABLED, som er fjernet og gir kompileringsfeil

--- src/test/java/no/nav/tag/kontakt/oss/metrics/MetricsListenersTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/metrics/MetricsListenersTest.java
@@ -14,7 +14,6 @@ public class MetricsListenersTest {
         //Svært enkel test for å kunne observere hvordan logglinjen vil se ut, og for å verifisere at enkelte data kan være null
         new MetricsListeners().gsakOppgaveSendt(new GsakOppgaveSendt(new GsakOppgaveService.Behandlingsresultat(OppgaveStatus.OK, 1), null));
         new MetricsListeners().gsakOppgaveSendt(new GsakOppgaveSendt(new GsakOppgaveService.Behandlingsresultat(OppgaveStatus.FEILET, null), null));
-        new MetricsListeners().gsakOppgaveSendt(new GsakOppgaveSendt(new GsakOppgaveService.Behandlingsresultat(OppgaveStatus.DISABLED, null), null));
         new MetricsListeners().gsakOppgaveSendt(new GsakOppgaveSendt(new GsakOppgaveService.Behandlingsresultat(OppgaveStatus.OK, 1), TestData.gsakRequest()));
     }
 }
