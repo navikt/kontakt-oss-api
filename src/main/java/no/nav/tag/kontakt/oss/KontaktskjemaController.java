90e76aa37fe29fef765c46e55276b5c5d3ee9169
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-14 14:05:25 +0200

    Legg feltvalidering sammen med eksisterende validering

--- src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
+++ src/main/java/no/nav/tag/kontakt/oss/KontaktskjemaController.java
@@ -4,29 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.regex.Pattern;
-
-import static no.bekk.bekkopen.org.OrganisasjonsnummerValidator.isValid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class KontaktskjemaController {
     private final KontaktskjemaService kontaktskjemaService;
-
-    private final static String LATIN = "a-zA-Z \\-–'._)(/";
-    private final static String SAMISK = "ÁáČčĐđŊŋŠšŦŧŽž";
-    private final static String NORSK = "æøåÆØÅ";
-
-    private final static String VANLIGE_BOKSTAVER = LATIN + SAMISK + NORSK;
-    private final static String SIFRE = "0-9";
-    private final static String EPOSTTEGN = "\\.@+";
-    private final static String AKSENTER = "ëÿüïöäéúíóáèùìòàêûîôâõãñËŸÜÏÖÄÉÚÍÓÁÈÙÌÒÀÊÛÎÔÂÕÃÑ";
-
-    private final static Pattern RAUS_TEKST = Pattern.compile("^[" + VANLIGE_BOKSTAVER + SIFRE + AKSENTER + "]*$");
-    private final static Pattern EPOST = Pattern.compile("^[" + VANLIGE_BOKSTAVER + SIFRE + AKSENTER + EPOSTTEGN + "]*$");
-    private final static Pattern SIFRE_MELLOMROM_OG_PLUSS = Pattern.compile("^[" + SIFRE + "+ " + "]*$");
 
     @Autowired
     public KontaktskjemaController(
@@ -43,34 +28,7 @@ public class KontaktskjemaController {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
 
-        validerKontaktskjema(kontaktskjema);
         kontaktskjemaService.lagreKontaktskjema(kontaktskjema);
         return ResponseEntity.ok(HttpStatus.OK);
-    }
-
-    private void validerKontaktskjema(Kontaktskjema kontaktskjema) {
-        validerSkjemafelt(kontaktskjema.getBedriftsnavn(), RAUS_TEKST);
-        validerSkjemafelt(kontaktskjema.getFornavn(), RAUS_TEKST);
-        validerSkjemafelt(kontaktskjema.getEtternavn(), RAUS_TEKST);
-        validerSkjemafelt(kontaktskjema.getFylke(), RAUS_TEKST);
-        validerSkjemafelt(kontaktskjema.getKommune(), RAUS_TEKST);
-        validerSkjemafelt(kontaktskjema.getTelefonnr(), SIFRE_MELLOMROM_OG_PLUSS);
-        validerSkjemafelt(kontaktskjema.getEpost(), EPOST);
-        validerOrgnr(kontaktskjema.getOrgnr());
-    }
-
-    private void validerSkjemafelt(String felt, Pattern skalBareInneholde) {
-        if (!skalBareInneholde.matcher(felt).matches()) {
-            String feil = "Skjemafelt \"" + felt + "\" må passe med det regulære uttrykket \"" + skalBareInneholde.pattern() + "\"";
-
-            log.error(feil);
-            throw new BadRequestException(feil);
-        }
-    }
-
-    private void validerOrgnr(String orgnr) {
-        if (!isValid(orgnr)) {
-            throw new BadRequestException("Orgnr " + orgnr + " er ugyldig");
-        }
     }
 }
--- src/test/java/no/nav/tag/kontakt/oss/KontaktskjemaControllerTest.java
+++ src/test/java/no/nav/tag/kontakt/oss/KontaktskjemaControllerTest.java
@@ -1,10 +1,5 @@
 package no.nav.tag.kontakt.oss;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.FylkesinndelingMedNavEnheter;
-import no.nav.tag.kontakt.oss.fylkesinndelingMedNavEnheter.KommuneEllerBydel;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,11 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static no.nav.tag.kontakt.oss.testUtils.TestData.*;
+import static no.nav.tag.kontakt.oss.testUtils.TestData.kontaktskjema;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
@@ -53,99 +44,4 @@ public class KontaktskjemaControllerTest {
         assertThat(kontaktskjemaController.meldInteresse(kontaktskjema()).getStatusCode(), is(HttpStatus.OK));
     }
 
-    @Test
-    @SneakyThrows
-    public void meldInteresse__skal_returnere_hvis_kontaktskjema_er_gyldig() {
-        Kontaktskjema gyldigKontaktskjema = kontaktskjemaBuilder()
-                .fornavn("Per")
-                .etternavn("Persén")
-                .bedriftsnavn("Årvõll Øks3sk4ft")
-                .epost("hei@årvoll.øks3-sk4ft.no")
-                .telefonnr("+47 99 99 99 99")
-                .orgnr("979312059")
-                .build();
-
-        assertThat(kontaktskjemaController.meldInteresse(gyldigKontaktskjema).getStatusCode(), is(HttpStatus.OK));
-    }
-
-    @SneakyThrows
-    @Test(expected = BadRequestException.class)
-    public void meldInteresse__skal_returnere_400_ved_ugyldig_bedriftsnavn() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .bedriftsnavn("$jokoladefabrikken")
-                .build();
-
-        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema).getStatusCode(), is(HttpStatus.BAD_REQUEST));
-    }
-
-    @SneakyThrows
-    @Test(expected = BadRequestException.class)
-    public void meldInteresse__skal_returnere_400_ved_ugyldig_telefonnumer() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .telefonnr("abcde")
-                .build();
-
-        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema).getStatusCode(), is(HttpStatus.BAD_REQUEST));
-    }
-
-    @SneakyThrows
-    @Test(expected = BadRequestException.class)
-    public void meldInteresse__skal_returnere_400_ved_ugyldig_epost() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .epost("hei@$jokoladefabrikken.no")
-                .build();
-
-        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema).getStatusCode(), is(HttpStatus.BAD_REQUEST));
-    }
-
-    @SneakyThrows
-    @Test(expected = BadRequestException.class)
-    public void meldInteresse__skal_returnere_400_ved_ugyldig_orgnummer() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .orgnr("abcde")
-                .build();
-
-        assertThat(kontaktskjemaController.meldInteresse(ugyldigKontaktskjema).getStatusCode(), is(HttpStatus.BAD_REQUEST));
-    }
-
-    @SneakyThrows
-    @Test
-    public void meldInteresse__skal_akseptere_understrek_i_epost() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .epost("hei_hei@nav.no")
-                .build();
-        kontaktskjemaController.meldInteresse(ugyldigKontaktskjema);
-    }
-
-    @SneakyThrows
-    @Test
-    public void meldInteresse__skal_akseptere_skråstrek_og_parenteser_i_bedriftsnavn() {
-        Kontaktskjema ugyldigKontaktskjema = kontaktskjemaBuilder()
-                .bedriftsnavn("Mark AS (egen bedrift) / Krok ENK (konas bedrift)")
-                .build();
-        kontaktskjemaController.meldInteresse(ugyldigKontaktskjema);
-    }
-
-    @SneakyThrows
-    @Test
-    public void meldInteresse_skal_ta_imot_alle_kommuner() {
-        hentAlleKommunenavnFraMock().forEach(kommune -> {
-            kontaktskjemaController.meldInteresse(kontaktskjemaBuilder().kommune(kommune).build());
-        });
-    }
-
-    @SneakyThrows
-    private List<String> hentAlleKommunenavnFraMock() {
-        String fylkesinndelingJson = lesFil("mock/fylkesinndeling.json");
-        FylkesinndelingMedNavEnheter fylkesinndeling = new FylkesinndelingMedNavEnheter(
-                new ObjectMapper().readValue(fylkesinndelingJson, new TypeReference<Map<String, List<KommuneEllerBydel>>>() {})
-        );
-        return fylkesinndeling
-                .getFylkeTilKommuneEllerBydel()
-                .values()
-                .stream()
-                .flatMap(List::stream)
-                .map(KommuneEllerBydel::getNavn)
-                .collect(Collectors.toList());
-    }
 }
