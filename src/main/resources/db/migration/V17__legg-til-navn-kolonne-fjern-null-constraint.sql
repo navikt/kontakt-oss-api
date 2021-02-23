46dda486c249d65b4de42db546906b48e40e88c4
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-02-23 10:08:45 +0100

    flytt fjerning av nullconstarint på navn inn i egen fil

--- src/main/resources/db/migration/V17__legg-til-navn-kolonne-fjern-null-constraint.sql
+++ src/main/resources/db/migration/V17__legg-til-navn-kolonne-fjern-null-constraint.sql
@@ -1,3 +1,1 @@
 alter table kontaktskjema add navn varchar(255);
-alter table kontaktskjema alter column etternavn drop not null;
-alter table kontaktskjema alter column fornavn drop not null;
---
+++ src/main/resources/db/migration/V19__fjern-null-constraint-paa-fornavn-etternavn.sql
@@ -0,0 +1,2 @@
+alter table kontaktskjema alter column etternavn drop not null;
+alter table kontaktskjema alter column fornavn drop not null;
