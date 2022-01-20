c0c67d7ef8ee233cc1c144de08e59925993623e5
havardmj <Havard.Myrvold.Johannessen@nav.no>
2022-01-20 14:09:00 +0100

    opprettet KontaktskjemaKafkaProducer

--- src/main/java/no/nav/arbeidsgiver/kontakt/oss/kafka/KontaktskjemaKafkaProducer.java
+++ src/main/java/no/nav/arbeidsgiver/kontakt/oss/kafka/KontaktskjemaKafkaProducer.java
@@ -1,11 +1,22 @@
 package no.nav.arbeidsgiver.kontakt.oss.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnProperty("kontakt-oss.kafka.enabled")
 @Component
 @Slf4j
 public class KontaktskjemaKafkaProducer {
-    // private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KontaktskjemaKafkaProducer(@Qualifier("producerKafkaTemplate") KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    private void publiserMelding() {
+
+    }
 }
