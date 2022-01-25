9e6836dd9dad1bfd47d297fd7636c4caf6511c3a
havardmj <Havard.Myrvold.Johannessen@nav.no>
2022-01-25 12:08:36 +0100

    lagt til namespace paa topic

--- src/main/java/no/nav/arbeidsgiver/kontakt/oss/kafka/Topics.java
+++ src/main/java/no/nav/arbeidsgiver/kontakt/oss/kafka/Topics.java
@@ -1,5 +1,5 @@
 package no.nav.arbeidsgiver.kontakt.oss.kafka;
 
 public class Topics {
-    public static final String KONTAKTSKJEMA = "arbeidsgiver-kontaktskjema";
+    public static final String KONTAKTSKJEMA = "team-innhold.arbeidsgiver-kontaktskjema";
 }
